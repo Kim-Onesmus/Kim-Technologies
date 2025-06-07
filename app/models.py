fc61ed8aa5b9572739383cab8463ea2920e6d798
Kim-Onesmus <kimones1122@gmail.com>
2025-06-07 15:10:52 +0300

    updates

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ class Testimonial(models.Model):
     organization = models.CharField(max_length=255)
     role = models.CharField(max_length=255)
     testimony = models.TextField()
+    rating = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='testimonials/', blank=True, null=True)
     is_published = models.BooleanField(default=False)
     date_created = models.DateTimeField(auto_now_add=True)
